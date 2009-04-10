2ba143e7bcf87d581a02dc1ffce82f8b30c96844
rasmus@mindplay.dk <rasmus@mindplay.dk@90d377ba-d246-0410-91ff-3da204c49a94>
2009-04-10 16:53:23 +0000

    prepare for release 1.0

--- test/test_require.php
+++ test/test_require.php
@@ -1,5 +1,8 @@
 <?php
 
+if (basename(__FILE__) == basename($_SERVER['PHP_SELF']))
+  die('This script is included from one of the other test scripts');
+
 define("TEST_VALUE", "This is the constant TEST_VALUE, defined in 'test_require.php'");
 
 class TestClass {
