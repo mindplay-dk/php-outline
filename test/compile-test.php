98d85c4cfaf96bc3df3a240506fcb3662f4b48f8
rasmus@mindplay.dk <rasmus@mindplay.dk@90d377ba-d246-0410-91ff-3da204c49a94>
2009-10-16 19:32:54 +0000

    fix trace messages

--- test/compile-test.php
+++ test/compile-test.php
@@ -6,9 +6,14 @@ function outline_function_testfunc($value) {
   return 'The time is: '.date('H:i:s',$value);
 }
 
-$engine = new Outline();
+function OutlineDebug($msg) {
+	echo "<div style=\"color:#f00\"><strong>Outline</strong>: $msg</div>";
+}
+
+$engine = new Outline(array('trace_callback' => 'OutlineDebug'));
 
 $engine->compile(
   dirname(__FILE__).'/templates/test.tpl.html',
-  dirname(__FILE__).'/compiled/test.tpl.php'
+  dirname(__FILE__).'/compiled/test.tpl.php',
+  true // force recompile
 );
